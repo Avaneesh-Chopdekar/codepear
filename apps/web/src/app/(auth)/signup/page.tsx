bd2540df16a46691988ded3b17dbea09f825d2cb
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-09-06 23:24:27 +0530

    created signup screen

---
+++ apps/web/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+import React, { FormEvent, useState } from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+
+export default function SignupPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log({ name, email, password });
+  }
+  function handleReset(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <main className="flex justify-center items-center mt-12">
+      <Card className="w-[400px]">
+        <CardHeader className="text-center text-2xl">Signup</CardHeader>
+        <CardDescription className="text-center">
+          Create a new account
+        </CardDescription>
+        <CardContent>
+          <form
+            onSubmit={handleSubmit}
+            onReset={handleReset}
+            className="flex flex-col items-center gap-4"
+          >
+            <Input
+              type="text"
+              placeholder="Name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="flex gap-2 items-center">
+              <Button type="submit">Signup</Button>
+              <Button variant="outline" type="reset">
+                Reset
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="w-full flex justify-center">
+          <p className="text-center">
+            Already have an account?
+            <Link href="/login" className="text-primary underline pl-1.5">
+              Log in
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
