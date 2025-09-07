a417e37f2fe361546b8e6bf506bacb0e5bb42ab8
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-09-07 18:22:00 +0530

    implemented layout structure for session page with monaco editor

--- apps/web/src/app/sessions/[id]/page.tsx
+++ apps/web/src/app/sessions/[id]/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
+import { toast } from "sonner";
+import Editor from "@monaco-editor/react";
+
 import useAuth from "@/hooks/use-auth";
+import { Button } from "@/components/ui/button";
 
 //* Demo Session Page: /sessions/ky1zsz
 
@@ -9,6 +13,8 @@ export default function SessionPage() {
   const { id: sessionId } = useParams();
   const router = useRouter();
   const auth = useAuth();
+
+  const [code, setCode] = useState("");
 
   const [hydrated, setHydrated] = useState(false);
   useEffect(() => {
@@ -21,5 +27,52 @@ export default function SessionPage() {
     }
   }, [hydrated, auth, router]);
 
-  return <div>Session {sessionId}</div>;
+  function handleExit() {
+    router.push("/dashboard");
+  }
+
+  function handleCopySessionId() {
+    navigator.clipboard.writeText(
+      `${window.location.origin}/sessions/${sessionId}`
+    );
+    toast("Link copied to clipboard");
+  }
+
+  return (
+    <main className="h-[calc(100vh-64px)] w-full overflow-y-hidden">
+      <div className="flex justify-between items-center border-b py-2 px-8">
+        <h1>
+          Session:{" "}
+          <Button variant={"link"} onClick={handleCopySessionId}>
+            {sessionId}
+          </Button>
+        </h1>
+        {/* TODO: Add timer */}
+        <Button variant="destructive" onClick={handleExit}>
+          Exit
+        </Button>
+      </div>
+
+      <div className="w-full h-full flex">
+        <div className="flex-1">
+          <Editor
+            defaultLanguage="javascript"
+            defaultValue="console.log('hello world');"
+            value={code}
+            onChange={(value) => setCode(value || "")}
+            options={{
+              automaticLayout: true,
+              minimap: {
+                enabled: false,
+              },
+            }}
+          />
+        </div>
+        <div id="tabs" className="flex-1">
+          <div>Problem Description</div>
+          <div>Video Call and Chat</div>
+        </div>
+      </div>
+    </main>
+  );
 }
